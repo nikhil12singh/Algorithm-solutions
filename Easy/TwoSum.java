ce425d5a0c5fff69c4241c4fa5aabcc91800eb70
Nikhil Kumar Singh <37964316+nikhil12singh@users.noreply.github.com>
2018-07-01 00:06:46 -0700

    Update TwoSum.java

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
-		
+		//Method 1
+		/*
 		int n = nums.length;
 		boolean flag = false;
 		int output[] = new int[2];
@@ -23,6 +24,62 @@ public class TwoSum {
 				}
 			}
 		}
+		return output;
+		*/
+		
+		// Method 2
+		int i = 0;
+		int n = nums.length;
+		int nums1[] = new int[n];
+		
+		for(int k=0;k<n;k++)
+			nums1[k] = nums[k];
+		
+		Arrays.sort(nums);
+		
+		int output[] = new int[2];
+		int num1 = 0, num2 = 0;
+		int j = n-1;
+		
+		while(i<j)
+		{
+			int sum = nums[i]+nums[j];
+			//System.out.println(nums[i]+" "+nums[j]);
+			if(sum == target)
+			{
+				num1 = nums[i];
+				num2 = nums[j];
+				break;
+			}
+			else if(sum<target)
+				i++;
+			else
+				j--;
+		}
+		int count = 0;
+		//System.out.println(num1+" "+num2);
+		boolean flag = false;
+		for(int k=0;k<n;k++)
+		{
+			if(count==2)
+				break;
+			if(nums1[k]==num1 && !flag)
+				{
+				flag = true;
+				output[0] = k;
+				//System.out.println(k+" 1st"+" "+output[0]);
+				count++;
+				}
+			
+			else if(nums1[k]==num2)
+			{
+			output[1] = k;
+			//System.out.println(k+" 2nd"+ output[1]);
+			count++;
+			}
+		}
+		//System.out.println(output[0]);
+		
 		return output;
     }
 }
