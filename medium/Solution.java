d3df3f2102bdebc19b02f6fbf24fce9befe582fa
Nikhil Kumar Singh <37964316+nikhil12singh@users.noreply.github.com>
2018-07-26 21:39:36 -0700

    Add Numbers

--- medium/Solution.java
+++ medium/Solution.java
@@ -1,41 +1,31 @@
 
 public class Solution {
 
-   static int count(int n, int k)
-   {
-	   int num = factorial(n, k);
-	   int den = factorial(k);
-	   
-	   int res = num/den;
-	   
-	   return res;
-   }
-    
-   static int factorial(int n, int k)
-   {
-       int res = 1, i;
-    
-       for (i=n; i>=(n-k+1); i--)
-       {
-    	   res *= i;
-       }
-    
-       return res;
-   }
-   
-   static int factorial(int n)
-   {
-       int res = 1, i;
-       for (i=2; i<=n; i++)
-           res *= i;
-       return res;
-   }
-    
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+        ListNode p = dummy, p1 = l1, p2 = l2;
+        int carrier = 0;
+        while (p1 != null || p2 != null || carrier != 0) {
+            int sum = 0;
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            sum += carrier;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            carrier = sum / 10;
+        }
+        return dummy.next;
+    }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
-	System.out.println(count(100,2));
 	}
 
 }
